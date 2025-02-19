c242d285ca5682ac482562af18f004ddd49d08d5
Nikita Amelin <nikita.amelin@student.griffith.ie>
2025-02-19 21:21:52 +0000

    task 2 part 2

--- Amelin_Nikita_3121276_ws1/src/test/java/tasks/WordsTest.java
+++ Amelin_Nikita_3121276_ws1/src/test/java/tasks/WordsTest.java
@@ -9,6 +9,7 @@ import org.junit.BeforeClass;
 import org.junit.Test;
 
 public class WordsTest {
+	
 	char[] letters1 = {'m','a','r','s'};
 	char[] letters2 = {'j','u','p','i','t','e','r'};
 	char[] letters3 = {'s','a','t','u','r','n'};
@@ -19,16 +20,23 @@ public class WordsTest {
 
 	@Test
 	public void testContains() {
-		fail("Not yet implemented");
+		assertTrue(word1.contains('m'));
+		assertTrue(word2.contains('j'));
+		assertTrue(word3.contains('s'));
 	}
 	
 	@Test
 	public void testLength() {
-		fail("Not yet implemented");
+		assertEquals(letters1.length, word1.length());
+		assertEquals(letters2.length, word2.length());
+		assertEquals(letters3.length, word3.length());
 	}
 	
 	@Test
 	public void testNotNull() {
-		fail("Not yet implemented");
+		assertNotNull(word1.getLetters());
+		assertNotNull(word2.getLetters());
+		assertNotNull(word3.getLetters());
+
 	}
 }
